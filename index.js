cb57ca0b5b02d087d00666cf6d8bee7a06aac0ed
Sergio Lepore <sergio.d.lepore@gmail.com>
2015-02-08 19:34:20 -0300

    add basic structure

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-require("6to5/polyfill"); // load polyfills and stuff
+if (!global._6to5Polyfill) {
+  require("6to5/polyfill"); // load polyfills and stuff
+}
 
 module.exports = require('./dist/tyson.js');
---
+++ test/index.js
@@ -0,0 +1,7 @@
+describe('Tyson Tests:', function(){
+  before(require('./scripts/before.js'))
+
+  require('./scripts/basic.js')
+
+  after(require('./scripts/after.js'))
+})
